210ebdd88d8c84b834367f2e880994321b284053
Aritra Koley <koley.aritra824@gmail.com>
2024-07-06 16:56:19 +0530

    Basic sidebar example

--- src/_common/Icons.jsx
+++ src/_common/Icons.jsx
@@ -10,7 +10,41 @@ export const ArrowLeftIcon = (
     <path
       strokeLinecap="round"
       strokeLinejoin="round"
-      d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18"
+      d="m5.25 4.5 7.5 7.5-7.5 7.5m6-15 7.5 7.5-7.5 7.5"
+    />
+  </svg>
+);
+
+export const Bars3Icon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5"
+    />
+  </svg>
+);
+
+export const CloseIcon = (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    fill="none"
+    viewBox="0 0 24 24"
+    strokeWidth={1.5}
+    stroke="currentColor"
+    className="size-6"
+  >
+    <path
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      d="M6 18 18 6M6 6l12 12"
     />
   </svg>
 );
--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,22 +1,29 @@
-import React, { useState } from "react";
-import { ArrowLeftIcon } from "../_common/Icons";
+import React, { useEffect, useState } from "react";
+import { ArrowLeftIcon, Bars3Icon } from "../_common/Icons";
 
 const SideBar = () => {
-  const [sidePaneIsOpen, setSidePaneIsOpen] = useState(true);
+  const [sidePaneIsOpen, setSidePaneIsOpen] = useState(false);
+  useEffect(() => {
+    const screenWidth = window.innerWidth;
+    if (screenWidth >= 768) {
+      setSidePaneIsOpen(true);
+    }
+  }, []);
+
   return (
     <article id="container" className="flex w-full h-screen bg-gray-100">
       <section
         id="sidebar"
         className={
-          "bg-gray-900 flex flex-col transition-all ease-in-out duration-500" 
-          + `${sidePaneIsOpen ? " w-[20rem]" : " w-[3rem]"}`
+          "bg-gray-900 flex flex-col md:bg-sky-900  transition-all ease-in-out duration-500 max-w-[25%] min-w-[20rem] z-20" +
+          `${sidePaneIsOpen ? " translate-x-0" : " -translate-x-full"}`
         }
       >
-        <div className="flex justify-end w-full">
+        <div className=" flex justify-end w-full">
           <button
             className={
               "w-10 h-10 p-2 mx-1 text-gray-100 transition-all ease-in-out duration-500" +
-              `${sidePaneIsOpen ? "" : " rotate-180"}`
+              `${sidePaneIsOpen ? " rotate-180" : ""}`
             }
             onClick={() => setSidePaneIsOpen(!sidePaneIsOpen)}
           >
@@ -24,7 +31,16 @@ const SideBar = () => {
           </button>
         </div>
       </section>
-      <section id="content"></section>
+      <section id="content">
+        <button
+          className={
+            "absolute top-0 left-0 w-10 h-10 p-2 mx-1 text-gray-900 transition-all ease-in-out duration-500 z-10"
+          }
+          onClick={() => setSidePaneIsOpen(!sidePaneIsOpen)}
+        >
+          {Bars3Icon}
+        </button>
+      </section>
     </article>
   );
 };
